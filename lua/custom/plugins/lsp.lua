681cfca5827b23d249fadd9abbcec6cf22834e4c
Roman Ivashkin <st086961@student.spbu.ru>
2025-02-06 12:04:39 +0300

    ADD: add python lsp

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -79,7 +79,7 @@ return {
       local servers = {
         -- clangd = {},
         -- gopls = {},
-        -- pyright = {},
+        pyright = {},
         -- rust_analyzer = {},
         tailwindcss = {},
         ts_ls = {},
