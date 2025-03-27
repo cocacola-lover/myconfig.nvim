aec638e8b5e2a129e206f9970de3dc18fa93b65f
Roman Ivashkin <st086961@student.spbu.ru>
2025-03-27 09:46:35 +0300

    ADD: outline.nvim

--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -76,7 +76,7 @@ return {
 
       -- See `:help telescope.builtin`
       vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
-      vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
+      vim.keymap.set('n', '<leader><space>', require('telescope.builtin').find_files, { desc = '[ ] Search Files' })
       vim.keymap.set('n', '<leader>/', function()
         -- You can pass additional configuration to telescope to change theme, layout, etc.
         require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
@@ -85,24 +85,17 @@ return {
         })
       end, { desc = '[/] Fuzzily search in current buffer' })
 
-      vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
-      vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
-      -- vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
+      vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
+      vim.keymap.set('n', '<leader>sb', require('telescope.builtin').buffers, { desc = '[S]earch existing [B]uffers' })
       vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
       vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
       vim.keymap.set('n', '<leader>sG', ':LiveGrepGitRoot<cr>', { desc = '[S]earch by [G]rep on Git Root' })
       vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
-      vim.keymap.set('n', '<leader>sS', function()
+      vim.keymap.set('n', '<leader>ss', function()
         require('telescope.builtin').lsp_dynamic_workspace_symbols({
           symbols = { "Function", "Method", "Class", "Interface", "Struct", "Enum" }
         })
       end, { desc = '[S]earch [S]ymbols' })
-
-      vim.keymap.set('n', '<leader>ss', function()
-        require('telescope.builtin').lsp_document_symbols({
-          symbols = { "Function", "Method", "Class", "Interface", "Struct", "Enum", "Constant", "Variable" }
-        })
-      end, { desc = '[S]earch document [s]ymbols' })
       vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]esume' })
     end
   }
--- lua/custom/plugins/widgets.lua
+++ lua/custom/plugins/widgets.lua
@@ -46,5 +46,21 @@ return {
       require("nvim-tree").setup {}
       vim.api.nvim_set_keymap("n", "<leader>e", ":NvimTreeFindFileToggle<CR>", { noremap = true, silent = true, desc = '[E]xplore' })
     end,
-  }
+  },
+  {
+    "hedyhli/outline.nvim",
+    config = function()
+      -- Example mapping to toggle outline
+      vim.keymap.set("n", "<leader>o", "<cmd>Outline<CR>",
+        { desc = "Toggle Outline" })
+
+      require("outline").setup {
+        keymaps = {
+          goto_location = '<S-Cr>',
+          goto_and_close = '<Cr>'
+        }
+        -- Your setup opts here (leave empty to use defaults)
+      }
+    end,
+  },
 }
