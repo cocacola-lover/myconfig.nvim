f099bd6445dbe6172a47a42ae8f69fd06d13228b
Roman Ivashkin <st086961@student.spbu.ru>
2025-03-28 11:25:27 +0300

    ADD: center cursor on <C-d> and <C-u>

--- init.lua
+++ init.lua
@@ -76,6 +76,10 @@ vim.o.termguicolors = true
 -- See `:help vim.keymap.set()`
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
+-- Custom remaps
+vim.keymap.set('n', '<C-d>', '<C-d>zz', { noremap = true })
+vim.keymap.set('n', '<C-u>', '<C-u>zz', { noremap = true })
+
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
